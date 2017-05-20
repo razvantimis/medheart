cb8085884b50de1fd0db98ddf9983d6b4969be07
Razvan Timis <razvantimis@192-168-0-103.rdsnet.ro>
2017-05-20 12:26:03 +0300

    Add scale and precent

--- src/predictDisease/components/Heart.js
+++ src/predictDisease/components/Heart.js
@@ -1,47 +1,64 @@
 import React, { Component, PropTypes } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 class Heart extends Component {
   static propTypes = {
-    style: PropTypes.object
+    style: PropTypes.object,
+    precent: PropTypes.string.isRequired,
+    scale: PropTypes.number.isRequired
   }
   render(){
+    const {precent, scale } = this.props
+    const styles = {
+      heart: {
+        width: 20*scale,
+        height: 20*scale,
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+   
+      },
+      heartShape: {
+        width: 12*scale,
+        height: 18*scale,
+        position: 'absolute',
+        top: 0,
+        borderTopLeftRadius: 6*scale,
+        borderTopRightRadius: 6*scale,
+        backgroundColor: 'red',
+        borderColor: 'red',
+        borderWidth: 2,
+        borderStyle: 'solid',
+        backgroundSize: 75
+      },
+      leftHeart: {
+        transform: [
+        {rotate: '-45deg'}
+        ],
+        left: 1.9*scale,
+   
+      },
+      rightHeart: {
+        transform: [
+        {rotate: '45deg'}
+        ],
+        right: 1.9*scale,
+     
+      },
+      text: {
+        fontSize: 32, 
+        color: '#fff'
+      }
+    }
     return (
     <View {...this.props} style={[styles.heart, this.props.style]}>
         <View style={[styles.leftHeart, styles.heartShape]} />
         <View style={[styles.rightHeart, styles.heartShape]} />
+        <Text style={styles.text}>{precent}%</Text>
     </View>
     );
   }
 }
-const styles = {
-  heart: {
-    width: 200,
-    height: 200
-   
-  },
-  heartShape: {
-    width: 120,
-    height: 180,
-    position: 'absolute',
-    top: 0,
-    borderTopLeftRadius: 60,
-    borderTopRightRadius: 60,
-    backgroundColor: '#EE1B24',
-  },
-  leftHeart: {
-    transform: [
-        {rotate: '-45deg'}
-    ],
-    left: 19,
-   
-  },
-  rightHeart: {
-    transform: [
-        {rotate: '45deg'}
-    ],
-    right: 19,
-     
-  }
-}
+
 
 export default Heart;
