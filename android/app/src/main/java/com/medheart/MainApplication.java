08b1ab118c88ebfbb963bf38ac0d12fda6f60eca
Razvan Timis <razvantimis@MacPro15s-MacBook-Pro.local>
2017-05-31 13:51:20 +0300

    Add react-native-svg and react-native-permission

--- android/app/src/main/java/com/medheart/MainApplication.java
+++ android/app/src/main/java/com/medheart/MainApplication.java
@@ -3,6 +3,7 @@ package com.medheart;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new ReactNativePermissionsPackage(),
             new SvgPackage(),
               new VectorIconsPackage(),
               new BlePackage(),
